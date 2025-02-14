2cfb958b982ebadb5e333c089b156e00e301814e
Nay-nay93 <greconaomi@gmail.com>
2025-02-14 18:54:45 +0000

    added typewriter plug in and basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Hello all");
+
+function generatePoem (event) {
+    event.preventDefault();
+
+}  new Typewriter("#poem", {
+    strings: "Poem will go here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+
+let formElement = document.querySelector("form");
+formElement.addEventListener("submit", generatePoem);
